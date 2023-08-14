889cc1a029552a5e4a24d77e70ef8dc76a33d5b7
kofirockerfeller <gpast2444@gmail.com>
2023-08-14 17:13:25 -0400

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>I am Gregory Oppong. A sophomore at Brandeis University pursuing Computer Science and Applied Mathematics</p>
+        <p>I am passionate about software applications and how they work.</p>
+        <p>I hope to pursue a career in Software Engineering</p>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <p>You can reach me at gregory.opon165@email.com</p>
+    </section>
+  );
+};
+
+export default Contact;
